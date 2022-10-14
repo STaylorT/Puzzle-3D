b273ae2d07e743e8e3e0ee51f0286dce55ecebab
Taylor Thomas <stth223@uky.edu>
2022-10-14 00:33:27 -0400

    begin level 2 and add togglable camera angles

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,22 +4,57 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+	public const int NUMBER_OF_CAMERAS = 3;
 	// A reference to the player object.
 	public GameObject Player;
-	
+		
 	// The distance between the camera and the player object.
-	private Vector3 Offset;
+	private Vector3 mainOffset;
+	private Vector3 povOffset;
+
+	private Camera[] sceneCameras = new Camera[NUMBER_OF_CAMERAS];
+	private int currCamera = 0;
+
 
 	// Start is called before the first frame update.
 	void Start()
 	{
+		Camera.GetAllCameras(sceneCameras);
 		Player = GameObject.Find("Player");
-		Offset = transform.position - Player.transform.position;
+		calculateOffsets();
+		
 	}
 
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.C)) {
+			currCamera = (currCamera + 1) % 3;
+     }
+	 for (int i = 0 ; i < NUMBER_OF_CAMERAS; i++) {
+		if (i == currCamera){
+			sceneCameras[i].enabled = true;
+		} else{
+			sceneCameras[i].enabled = false;
+		}
+	 }
+	}
 	// LateUpdate is called after Update but before rendering the frame.
 	void LateUpdate()
 	{
-		transform.position = Player.transform.position + Offset;
+		if (sceneCameras[currCamera].name == "Main Camera"){
+			sceneCameras[currCamera].transform.position = Player.transform.position + mainOffset;
+		} else if (sceneCameras[currCamera].name == "povCamera") {
+			sceneCameras[currCamera].transform.position = Player.transform.position + povOffset;
+		}
 	}
+
+	private void calculateOffsets()
+	{
+		GameObject mainCam = GameObject.Find("Main Camera");
+		GameObject povCam = GameObject.Find("povCamera");
+		mainOffset = mainCam.transform.position - Player.transform.position;
+		povOffset = povCam.transform.position - Player.transform.position;
+
+	}
+	
 }
